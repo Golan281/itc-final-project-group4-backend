ade8f1675b18501249437f88e4aeabf7f2b1b122
Golan Blumenkrantz <golan281@gmail.com>
2022-06-09 00:35:50 +0300

    last commit before branching

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
-const PORT = process.env.PORT || 4000;
-const HOST = `localhost`;
+const PORT = process.env.PORT || 8000;
+const HOST = `${process.env.HOST}` || `localhost`;
 
 
 app.listen(PORT,HOST, () => {

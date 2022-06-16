fbb3fef35a569489f38d329f78f6f353febe7a40
Bartooz <raphchetrit@gmail.com>
2022-06-16 19:05:58 +0300

    Raph part 3

--- models/mongooseModels/tabSchema.js
+++ models/mongooseModels/tabSchema.js
@@ -1,2 +1,37 @@
 // {tabName:'',desc:'',tabUrl:'',workSpaceId:'', workSpaceName:'', isArchived: bool default false, userId:''}
 //& built in _id & timestamps by mongoose
+
+const mongoose = require("mongoose");
+
+const tabSchema = new mongoose.Schema({
+  tabName: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+
+  tabURL: {
+    type: String,
+    required: true,
+  },
+  workSpaceID: {
+    type: String,
+    required: true,
+  },
+  workSpaceName: {
+    type: String,
+  },
+  isArchived: {
+    type: Boolean,
+    default: "false",
+  },
+  userID: {
+    type: String,
+  },
+});
+
+const Tab = mongoose.model("tabs", tabSchema);
+
+module.exports = { Tab };

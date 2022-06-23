645857d84787ab46c5d26c06508922ccfb852f99
johnathan Meerfeld <johnathanmeerf@gmail.com>
2022-06-23 18:19:48 +0300

    add/delete Tab functionalities work

--- controllers/db/tabController.js
+++ controllers/db/tabController.js
@@ -1,21 +1,65 @@
 const Dao = require("../db/db");
 const dao = new Dao();
-
+const { UserSchema } = require("../../models/mongooseModels/userSchema");
 const { Tab } = require("../../models/mongooseModels/tabSchema");
 const { WorkSpace } = require("../../models/mongooseModels/workspaceSchema");
 
 const createTab = async (req, res) => {
-  const tab = req.body;
+  const tab = req.body.newTab;
   const newTab = await Tab.create({ ...tab });
+  console.log(newTab);
+  const user = await UserSchema.findOne({
+    _id: req.body.userID,
+  });
+  const userID = user._id;
+  const workSpaceName = req.body.workSpaceName;
+  console.log(user);
+  const workSpaceArr = user?.userWorkSpaces;
+  dupes = workSpaceArr.find((space) => space === workSpaceName);
 
-  const workspace = await WorkSpace.findById(req.params.workSpaceId);
-  console.log(workspace);
+  if (workSpaceArr.length < 5) {
+    console.log("checking for dupes");
+    console.log(dupes);
+
+    if (dupes !== undefined) {
+      const workspace = await WorkSpace.findOne({
+        workSpaceName: workSpaceName,
+      });
+      workspace.currentUserTabs = [
+        ...new Set([...workspace.currentUserTabs, newTab]),
+      ];
+      await workspace.save();
+
+      res.json({ workspace });
+      return;
+    }
+
+    const addedWorkspace = await WorkSpace.create({
+      userID,
+      workSpaceName,
+    });
+
+    user.userWorkSpaces = [
+      ...user.userWorkSpaces,
+      addedWorkspace.workSpaceName,
+    ];
+    await user.save();
+  }
+
+  const workspace = await WorkSpace.findOne({
+    workSpaceName: workSpaceName,
+  });
+
+  if (!workspace) {
+    throw new Error("NO WORKSPACE FOUND");
+  }
+
   workspace.currentUserTabs = [
-    ...new Set([...workspace.currentUserTabs, newTab._id]),
+    ...new Set([...workspace.currentUserTabs, newTab]),
   ];
   await workspace.save();
 
-  res.send(`tab created: ${newTab}`);
+  res.json({ workspace });
 };
 
 const UpdateTab = async (req, res) => {
@@ -28,9 +72,37 @@ const UpdateTab = async (req, res) => {
 };
 
 const deleteTab = async (req, res) => {
-  const newTab = await Tab.findByIdAndDelete(req.params.tabId);
-  console.log(newTab);
-  res.send(`workspace name deleted: ${newTab}`);
+  const user = await UserSchema.findOne({
+    _id: req.body.userID,
+  });
+  // console.log("THIS FAR SO GOOD", user);
+  const userID = user._id;
+  const tabID = req.body.tabId;
+  // console.log(req.params);
+  const workSpaceName = req.body.workSpaceName;
+  const workspace = await WorkSpace.findOne({
+    workSpaceName: workSpaceName,
+  });
+  // console.log(workspace);
+  const ids = workspace.currentUserTabs.map((tab) => tab._id.toString());
+
+  const cleanWorkspace = workspace.currentUserTabs.filter(
+    (tab) => tab._id.toString() !== tabID
+  );
+  const badTab = workspace.currentUserTabs.find(
+    (tab) => tab._id.toString() !== tabID
+  );
+  console.log(cleanWorkspace);
+
+  workspace.currentUserTabs = cleanWorkspace;
+
+  await workspace.save();
+  await user.save();
+  await Tab.findByIdAndDelete({ _id: badTab._id });
+  // console.log(newTabs);
+  // console.log(newTab);
+
+  res.json({ workspace });
 };
 
 module.exports = {
